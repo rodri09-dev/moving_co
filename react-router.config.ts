852ee707e3c9e18255c2f2850fda61b8f259205b
Ruben Rodriguez <ruben@amalgam-inc.com>
2026-01-04 21:21:16 -0500

    added netlify

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,6 +2,6 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  // Disable SSR for static Netlify hosting
+  ssr: false,
 } satisfies Config;
